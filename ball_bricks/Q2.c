047d77550514bd8ea4130de0882b39a0b3d35e32
Sarvani Gudipati <sarvanigudipati2000j@gmail.com>
2022-03-13 21:57:53 +0530

    Ball Bricks Game

---
+++ ball_bricks/Q2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=7,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i==0 || j==0 || j==n-1)
+                printf(" W");
+            else if(i==n-1 && (j==1 ||j==2 || j==4 || j==5))
+                printf(" G");
+            else if(i==n-1 && j==3)
+                printf(" o");
+            else if((i==2 && (j==2 || j==3 || j==4)) || (i==3 && (j==2 || j==4)))
+                printf(" 1");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+    printf("Ball count is 3");
+
+
+    return 0;
+}
